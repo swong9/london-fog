b88c7cb37349ade41a31bd601e0069ee721cdc79
Eric Pai <ericpai@berkeley.edu>
2016-11-06 10:32:27 -0800

    create rudimentary edit system

--- sushi_go.py
+++ sushi_go.py
@@ -1,3 +1,8 @@
+DEFAULT_PROMPT = "--> "
+
+def get_players():
+	pass
+
 def go(players):
 	""" Keeps scores for a game of Sushi Go. Run a game by calling the function
 	and passing in the number of players, followed by each of the players'
@@ -8,7 +13,7 @@ def go(players):
 	"""
 	scores = {}
 	while players > 0:
-		name = get_input("Input the player's name")
+		name = get_input_type("Input the player's name:")
 		scores[name] = 0
 		players -= 1
 	round = 1
@@ -17,14 +22,14 @@ def go(players):
 		nonlocal scores
 		print("Beginning of round", round)
 		for name in scores.keys():
-			score = get_input("Give a score for {0}"\
+			score = get_input_type("Give a score for {0}:"\
 				.format(name), "int")
 			scores[name] = scores[name] + int(score)
 		print("End of round", str(round))
 		round += 1
 		if round > 3:
 			for name in scores.keys():
-				dessert_score = get_input("Input dessert score for {0}"\
+				dessert_score = get_input_type("Input dessert score for {0}:"\
 					.format(name), "int")
 				scores[name] = scores[name] + int(dessert_score)
 			highest_score = 0
@@ -43,14 +48,46 @@ def go(players):
 				print("The winner is {0} with a score of {1}!"\
 					.format(winner, highest_score))
 			for name in scores.keys():
-				print("{0}:{1}".format(name, scores[name])
+				print("{0}:{1}".format(name, scores[name]))
 			return "Game finished!"
 		else:
 			return play_round()
 
 	return play_round()
 
-def get_input(prompt, value_type="str"):
+def allow_edits(items, name="item"):
+	edit_name = "edit"
+	possible_cmds = ["yes"] + \
+		["{}{}".format(edit_name, i) for i,_ in enumerate(items, start=1)]
+	while True:
+		print("are these {0}s correct?".format(name))
+		for i, item in enumerate(items, start=1):
+			print("  ({0} {1})  {2}".format(name, i, item))
+		cmd = assert_input("type `yes` if correct, or `{0}1` to edit item 1, etc."\
+			.format(edit_name), possible_cmds)
+		if cmd == 'yes':
+			return items
+		edit_num = int(cmd[len(edit_name):])
+		value = get_input("enter a new value for {0} {1}".format(name, edit_num))
+		items[edit_num - 1] = value
+
+def assert_input(prompt, possible_values):
+	input_prompt = "{0}\npossible values: {1}".format(prompt, possible_values)
+	value = get_input(input_prompt)
+	while value not in possible_values:
+		print("{0}^ invalid input\n".format(" " * len(DEFAULT_PROMPT)))
+		value = get_input(input_prompt, print_after=False)
+	print()
+	return value
+
+def get_input(prompt, input_prompt=DEFAULT_PROMPT, print_after=True):
+	print(prompt)
+	value = input(input_prompt)
+	if print_after:
+		print()
+	return value
+
+def get_input_type(prompt, value_type="str"):
 	""" Gets input, and ensures it's of type `value_type`.
 		Currently only supports str and int
 
@@ -61,15 +98,14 @@ def get_input(prompt, value_type="str"):
 	5
 	"""
 	assert value_type in ["str", "int"], "{} type not supported".format(value_type)
-	value = input("{0}: ".format(prompt))
+	value = get_input(prompt)
 	if value_type == "str":
 		return value
 	while True:
 		try:
 			return int(value)
 		except:
-			value = input("{0} (must be int): ".format(prompt))
+			value = get_input("{0} (must be int): ".format(prompt))
 
 # if __name__ == '__main__':
-
 # 	go()
